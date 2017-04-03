4b9c2b21695fad10af9620581f57f2026bd4f100
eric-larkin <eal64@pitt.edu>
2017-04-03 18:09:59 -0400

    Update ParentClass.php

--- ParentClass.php
+++ ParentClass.php
@@ -2,5 +2,34 @@
 	// This is the file for the parent class
 
 	class ParentClass {
-		
+		protected $userName = "";
+		protected $songArtistsArray = array();
+
+		public function __construct($name) {
+				$this->userName = $name;
+		}
+
+		public function __clone() {
+			return $this;
+		}
+
+		public function addSong($song, $artist) {
+			$this->songArtistsArray[$song] = $artist;
+		}
+
+		public function removeSong($song) {
+			unset($this->songArtistsArray[$song]);
+		}
+
+		public function __toString() {
+			$returnable = "";
+			if(isset($this->songArtistsArray)) {
+				$returnable .= $this->userName . "'s playlist";
+				foreach ($this->songArtistsArray as $key => $value) {
+					$returnable .= $key . " -> " . $value;
+				}
+
+				return $returnable;
+			}
+		}
 	}
